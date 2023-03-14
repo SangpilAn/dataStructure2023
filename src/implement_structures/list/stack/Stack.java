244bbed041245ad4c5a7078e7d8b2b2a3cb15ab8
asp4303 <asp4303@gmail.com>
2023-03-14 21:44:06 +0900

    Stack Class 생성

---
+++ src/implement_structures/list/stack/Stack.java
@@ -0,0 +1,40 @@
+package implement_structures.list.stack;
+
+import interface_form.StackInterface;
+
+public class Stack<E> implements StackInterface<E> {
+    @Override
+    public E push(E item) {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public int search(Object value) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean empty() {
+        return false;
+    }
+}
