b0c64f7b7df77106ea9bee8b3b7330c4ff9f7e37
asp4303 <asp4303@gmail.com>
2023-04-09 16:54:47 +0900

    LinkedHashSet 구현 완료

---
+++ src/implement_structures/set/linkedHash/LinkedHashSet.java
@@ -0,0 +1,294 @@
+package implement_structures.set.linkedHash;
+
+import interface_form.Set;
+
+import java.util.Arrays;
+
+public class LinkedHashSet<E> implements Set<E> {
+
+    private static final int DEFAULT_CAPACITY = 1 << 4;
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    Node<E>[] table;
+    private int size;
+
+    private Node<E> head;
+    private Node<E> tail;
+
+    public LinkedHashSet(){
+        table = (Node<E>[]) new Node[DEFAULT_CAPACITY];
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    private static final int hash(Object key){
+        int hash;
+        if (key == null){
+            return 0;
+        }else {
+            return Math.abs(hash = key.hashCode()) ^ (hash >>> 16);
+        }
+    }
+
+    private void linkLastNode(Node<E> o){
+        Node<E> last = tail;
+
+        tail = o;
+
+        if (last == null){
+            head = o;
+        }else {
+            o.prevLink = last;
+            last.nextLink = o;
+        }
+    }
+
+    @Override
+    public boolean add(E e) {
+        return add(hash(e), e) == null;
+    }
+
+    private E add(int hash, E key) {
+        int idx = hash % table.length;
+
+        Node<E> newNode = new Node<>(hash, key, null);
+
+        if (table[idx] == null){
+            table[idx] = newNode;
+        }else {
+            Node<E> temp = table[idx];
+            Node<E> prev = null;
+
+            while (temp != null){
+
+                if (temp.hash == hash && (temp.key == key || temp.key.equals(key))){
+                    return key;
+                }
+                prev = temp;
+                temp = temp.next;
+            }
+
+            prev.next = newNode;
+        }
+        size++;
+
+        linkLastNode(newNode);
+
+        if (size >= LOAD_FACTOR * table.length){
+            resize();
+        }
+
+        return null;
+    }
+
+    private void resize() {
+        int newCapacity = table.length * 2;
+
+        Node<E>[] newTable = (Node<E>[]) new Node[newCapacity];
+
+        for (int i = 0; i < table.length; i++) {
+
+            Node<E> value = table[i];
+
+            if (value == null){
+                continue;
+            }
+
+            table[i] = null;
+
+            Node<E> nextNode;
+
+            while (value != null){
+                int idx = value.hash % newTable.length;
+
+                if (newTable[idx] != null){
+                    Node<E> tail = newTable[idx];
+
+                    while (tail.next != null){
+                        tail = tail.next;
+                    }
+
+                    nextNode = value.next;
+                    value.next = null;
+                    tail.next = value;
+                }else {
+                    nextNode = value.next;
+                    value.next = null;
+                    newTable[idx] = value;
+                }
+
+                value = nextNode;
+            }
+        }
+
+        table = null;
+        table = newTable;
+    }
+
+    private void unlinkNode(Node<E> o){
+        Node<E> prevNode = o.prevLink;
+        Node<E> nextNode = o.nextLink;
+
+        if (prevNode == null){
+            head = nextNode;
+        }else {
+            prevNode.nextLink = nextNode;
+            o.prevLink = null;
+        }
+
+        if (nextNode == null){
+            tail = prevNode;
+        }else {
+            nextNode.prevLink = prevNode;
+            o.nextLink = null;
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return remove(hash(o), o) != null;
+    }
+
+    private Object remove(int hash, Object key) {
+        int idx = hash % table.length;
+
+        Node<E> node = table[idx];
+        Node<E> removedNode = null;
+        Node<E> prev = null;
+
+        if (node == null){
+            return null;
+        }
+
+        while (node != null){
+
+            if (node.hash == hash && (node.key == key || node.key.equals(key))){
+
+                removedNode = node;
+
+                if (prev == null){
+                    table[idx] = node.next;
+                }else {
+                    prev.next = node.next;
+                }
+
+                unlinkNode(node);
+                node = null;
+
+                size--;
+                break;
+            }
+
+            prev = node;
+            node = node.next;
+        }
+
+        return removedNode;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        int idx = hash(o) % table.length;
+        Node<E> temp = table[idx];
+
+        while (temp != null){
+            if (o == temp.key || o.equals(temp.key)){
+                return true;
+            }
+
+            temp = temp.next;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        if (table != null && size > 0){
+            for (int i = 0; i < table.length; i++) {
+                table[i] = null;
+            }
+
+            size = 0;
+        }
+        head = tail = null;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o == this){
+            return true;
+        }
+
+        if (!(o instanceof LinkedHashSet)){
+            return false;
+        }
+
+        LinkedHashSet<E> oSet;
+
+        try {
+            oSet = (LinkedHashSet<E>) o;
+
+            if (oSet.size() != size){
+                return false;
+            }
+
+            for (int i = 0; i < oSet.table.length; i++) {
+                Node<E> oTable = oSet.table[i];
+
+                while (oTable != null){
+
+                    if (!contains(oTable)){
+                        return false;
+                    }
+
+                    oTable = oTable.next;
+                }
+            }
+
+        }catch (ClassCastException e){
+            return false;
+        }
+
+        return true;
+    }
+
+    public Object[] toArray(){
+
+        if (table == null || head == null){
+            return null;
+        }
+
+        Object[] ret = new Object[size];
+        int index = 0;
+
+        for (Node<E> x = head; x != null; x = x.next){
+            ret[index] = x.key;
+            index++;
+        }
+
+        return ret;
+    }
+
+    public <T> T[] toArray(T[] a){
+        Object[] copy = toArray();
+        if (a.length < size){
+            return (T[]) Arrays.copyOf(copy, size, a.getClass());
+        }
+        System.arraycopy(copy, 0, a, 0, size);
+
+        return a;
+    }
+}
